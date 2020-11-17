5d19554e17f8879787b6fc9d5ce7987222a97e5f
Adam Vander Pas <adam.vander.pas@ni.com>
2020-11-16 23:05:26 -0600

    Add SparkMaxSim lib and setup hacky conditional import

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
@@ -21,7 +22,7 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem(RobotBase.isReal());
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
@@ -43,7 +44,6 @@ public class RobotContainer {
    */
   private void configureButtonBindings() {
   }
-
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -7,18 +7,58 @@
 
 package frc.robot.subsystems;
 
+import java.lang.Class;
+import java.lang.reflect.InvocationTargetException;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+//import net.thefletcher.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+//import net.thefletcher.revrobotics.enums.MotorType;
 
 public class ExampleSubsystem extends SubsystemBase {
   /**
    * Creates a new ExampleSubsystem.
    */
-  public ExampleSubsystem() {
+  Object m_frontSparkMax;
+  String sparkMaxLib;
+  Class<?> motorTypeClass;
+
+  public ExampleSubsystem(boolean isReal) {
+    String sparkMaxMotorTypeLib;
+    if (isReal) {
+      sparkMaxLib = "com.revrobotics.CANSparkMax";
+      sparkMaxMotorTypeLib = "com.revrobotics.CANSparkMaxLowLevel.MotorType";
+    } else {
+
+      sparkMaxLib = "net.thefletcher.revrobotics.CANSparkMax";
+      sparkMaxMotorTypeLib = "net.thefletcher.revrobotics.enums.MotorType";
+    }
+
+    try {
+      motorTypeClass = this.getClass().getClassLoader().loadClass(sparkMaxMotorTypeLib);
+
+      m_frontSparkMax = this.getClass().getClassLoader().loadClass(sparkMaxLib)
+          .getDeclaredConstructor(new Class[] { Integer.TYPE, motorTypeClass })
+          .newInstance(new Object[] { 0, motorTypeClass.getEnumConstants()[1] });
+    } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+        | NoSuchMethodException | SecurityException | ClassNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
 
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    try {
+      m_frontSparkMax.getClass().getMethod("set", new Class[] { Double.TYPE }).invoke(m_frontSparkMax,
+          new Object[] { 0.5 });
+    } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException
+        | SecurityException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
 }
